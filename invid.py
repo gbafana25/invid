49a54a776a11c74a26e3f3ffe4e105f6e5aeee5d
gbafana25 <garethkm25@gmail.com>
2022-05-06 20:34:22 -0500

    files save with video title, date published in output

--- invid.py
+++ invid.py
@@ -13,11 +13,11 @@ def searchVideos():
 	p = r.json()
 	for e in range(len(p)):
 		try:
-			print(str(e) + ")", p[e]['title'] + "  | " + str(round(p[e]['lengthSeconds']/60)) + "min")
+			print(str(e) + ")", p[e]['title'] + "  | " + str(round(p[e]['lengthSeconds']/60)) + "min  | " + p[e]['publishedText'])
 		except:
 			pass
 	v = input("> ")
-	return p[int(v)]['videoId']
+	return (p[int(v)]['videoId'], p[int(v)]['title'])
 	
 
 
@@ -26,13 +26,13 @@ def popularVideos():
 	r = requests.get(BASE_URL+"/popular")
 	s = json.loads(r.text)
 	for i in range(len(s)):
-		print(str(i) + ")", s[i]['title'] + "  | " + str(round(s[i]['lengthSeconds']/60)) + "min")
+		print(str(i) + ")", s[i]['title'] + "  | " + str(round(s[i]['lengthSeconds']/60)) + "min  | " + s[i]['publishedText'])
 	v = input("> ")
-	return s[int(v)]['videoId']
+	return (s[int(v)]['videoId'], s[int(v)]['title'])
 
 
 def downloadVideo(l):
-	v = requests.get(BASE_URL+"/videos/"+l)
+	v = requests.get(BASE_URL+"/videos/"+l[0])
 	p = json.loads(v.text)
 	for t in range(len(p['formatStreams'])):
 		print(str(t) + ") " + p['formatStreams'][t]['resolution'])
@@ -41,7 +41,7 @@ def downloadVideo(l):
 	#print(url)
 	print("downloading video...")
 	raw = requests.get(url)
-	o = open("video.mp4", "wb+")
+	o = open(l[1]+".mp4", "wb+")
 	o.write(raw.content)
 	o.close()
 
