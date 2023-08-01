ade401d88beabe359024c586722ee9bdc9517378
Gareth M <gkm25@protonmail.com>
2023-08-01 15:22:43 -0500

    takes list of search terms and auto-downloads

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # invid
 
-Python script for getting videos from invidious instances.
+Python script for searching videos from invidious instances.
+
+## Usage
+
+- To search and see search results for videos: `./invid.py search`
+- Automatically download videos from a list of search terms: `./invid.py from-list`
+	- Create a file called `search-terms` in order to feed a list of searches to be automatically downloaded.
+	- add the `video` argument if you want the files to be kept as such
 
 
 ## TODO
--- invid.py
+++ invid.py
@@ -2,60 +2,85 @@
 import json
 import requests
 import sys
+import os
+import time
 
-BASE_URL = "https://inv.riverside.rocks/api/v1"
+# add list of instances
+#BASE_URL = "https://vid.puffyan.us/api/v1"
+BASE_URL = "https://invidious.slipfox.xyz/api/v1"
+
+# removes characters that cause ffmpeg command to fail
+def sanitizeTitle(name):
+	return name.replace(' ', '-').replace('&', '').replace('(', '').replace(')', '') 
+
+def convertToMp3(name, src):	
+	print(name)
+	print(src)
+	os.system("ffmpeg -i "+src+" "+name+".mp3")
 
 
-def searchVideos():
+def searchVideos(term):
+	# returns first video from search
+	if(term != None):
+		print("searching...")
+		r = requests.get(BASE_URL+"/search?q="+term)
+		p = r.json()	
+		return (p[0]['videoId'], p[0]['title'])
+
 	qu = input("Search> ")	
 	print("searching...")
 	r = requests.get(BASE_URL+"/search?q="+qu)
 	p = r.json()
+	count = 0
+	vid_list = []
 	for e in range(10):
 		try:
-			print(str(e) + ")", p[e]['title'] + "  | " + str(round(p[e]['lengthSeconds']/60)) + "min  | " + p[e]['publishedText'])
+			if(p[e]['type'] == "video"):
+				vid_list.append((count, e))
+				print(str(count) + ")", p[e]['title'] + "  | " + str(round(p[e]['lengthSeconds']/60)) + "min  | " + p[e]['publishedText'])
+				count += 1
 		except:
 			pass
+
 	v = input("> ")
-	return (p[int(v)]['videoId'], p[int(v)]['title'])
-	
-
-
-def popularVideos():
-	print("getting list of popular videos...")
-	r = requests.get(BASE_URL+"/popular")
-	s = json.loads(r.text)
-	for i in range(len(s)):
-		print(str(i) + ")", s[i]['title'] + "  | " + str(round(s[i]['lengthSeconds']/60)) + "min  | " + s[i]['publishedText'])
-	v = input("> ")
-	return (s[int(v)]['videoId'], s[int(v)]['title'])
-
+	for i in range(len(vid_list)):
+		if vid_list[i][0] == int(v):
+			return (p[vid_list[i][1]]['videoId'], p[vid_list[i][1]]['title'])
+		
 
 def downloadVideo(l):
 	v = requests.get(BASE_URL+"/videos/"+l[0])
-	p = json.loads(v.text)
-	#print(p['adaptiveFormats'])
-	for t in range(len(p['formatStreams'])):
-		print(str(t) + ") " + p['formatStreams'][t]['resolution'])
-	res = input("Select quality> ")
-	url = p['formatStreams'][int(res)]['url']
+	p = json.loads(v.text)	
+	# list index correlates to video quality 
+	# 0 - usually 144p
+
+	url = p['formatStreams'][len(p['formatStreams'])-1]['url']
 	print("downloading video...")
 	raw = requests.get(url)
 	ext = ".mp4"
-	with open("video"+ext, "wb+") as o:
+	filename = sanitizeTitle(l[1])
+	with open(filename+ext, "wb+") as o:
 		o.write(raw.content)
 		o.close()
 
+	if(len(sys.argv) == 2):
+		convertToMp3(filename, filename+ext)
+	elif(sys.argv[2] == "video"):
+		print("Keeping as .mp4...")
 
-
-if(len(sys.argv) == 2):
+if(len(sys.argv) >= 2):
 	if(sys.argv[1] == 'search'):
 		while True:
-			vid = searchVideos()
+			vid = searchVideos(None)
 			downloadVideo(vid)
-	elif(sys.argv[1] == 'popular'):
-		vid = popularVideos()
-		downloadVideo(vid)
+	elif(sys.argv[1] == 'from-list'):
+		with open("search-terms", "r") as searches:
+			for search in searches:
+				vid = searchVideos(search)
+				downloadVideo(vid)
+				print("5 second delay for rate limit")
+				time.sleep(5)
+	
 else:
 	print("type a command...")
 	exit(0)
